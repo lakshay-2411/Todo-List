dced4254e3f2803ed65bd01e2ce18e4dc26d2275
lakshay-2411 <lakshaynandwani06@gmial.com>
2024-07-11 13:35:12 +0530

    Set up MongoDB connection and added MongoDB schemas and models

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 
 const app = express();
-
-
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = new mongoose.Schema ({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your TodoList!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+(async function() {
+  try {
+    await Item.insertMany(defaultItems);
+    console.log("Successfully saved  default items to DB.");
+  } catch (err) {
+    console.log(err);
+  }
+})();
+
 app.get("/", function(req, res) {
 
   const day = date.getDate();
+
+  Item.find({})
+  .then((todoItems) => {
+    console.log(todoItems);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
   res.render("list", {
     listTitle: day,
